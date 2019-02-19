c15c15b0b06541a0377efa9614af4cbbd9e4aef3
Jan Misar <jan.misar@ackee.cz>
2019-02-19 17:34:49 +0100

    Create single view app.

---
+++ mi-ios-2019/mi-ios-2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mi-ios-2019
+//
+//  Created by Jan Misar on 19/02/2019.
+//  Copyright © 2019 ČVUT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
