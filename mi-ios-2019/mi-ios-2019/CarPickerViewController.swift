e34c7f47d1c0e88d12822890f2c2393a42dd798e
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-02-26 18:25:00 +0100

    Create empty `CarPickerVC`

---
+++ mi-ios-2019/mi-ios-2019/CarPickerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CarPickerViewController.swift
+//  mi-ios-2019
+//
+//  Created by Jakub Olejník on 26/02/2019.
+//  Copyright © 2019 ČVUT. All rights reserved.
+//
+
+import UIKit
+
+final class CarPickerViewController: UIViewController {
+    
+}
--- mi-ios-2019/mi-ios-2019/ViewController.swift
+++ mi-ios-2019/mi-ios-2019/ViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet weak var pickCarButton: UIButton!
     @IBOutlet weak var versionLabel: UILabel!
     @IBOutlet weak var buildNumberLabel: UILabel!
     
+    // MARK: - View life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // TODO: Localize when permission is granted
+        // navigationItem.title = L10n.Start.title
+        // pickCarButton.setTitle(L10n.Start.pickCar, for: .normal)
         
         let versionText = (Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String) ?? ""
         let buildNumberText = (Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String) ?? ""
@@ -22,7 +28,5 @@ class ViewController: UIViewController {
         versionLabel.text = "\(L10n.Start.appVersion): \(versionText)"
         buildNumberLabel.text = "\(L10n.Start.buildNumber): \(buildNumberText)"
     }
-
-
 }
 
