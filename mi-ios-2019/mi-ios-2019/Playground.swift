0608d2c3d24ef72adb00e54654c67bbf0c9cbcb3
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-02-26 23:10:22 +0100

    Add conditional protocol conformance example

---
+++ mi-ios-2019/mi-ios-2019/Playground.swift
@@ -0,0 +1,79 @@
+//
+//  Playground.swift
+//  mi-ios-2019
+//
+//  Created by Jakub Olejník on 26/02/2019.
+//  Copyright © 2019 ČVUT. All rights reserved.
+//
+
+import Foundation
+
+enum Playground {
+    static func play() {
+        let arrayOfAlmostEquatables = Animal.allCases
+        let arrayOfAlmostEquatables2 = Animal.allCases
+        let arrayOfAlmostEquatables3 = Array(Animal.allCases.prefix(1))
+        
+        print(arrayOfAlmostEquatables ~== arrayOfAlmostEquatables2)
+        print(arrayOfAlmostEquatables ~== arrayOfAlmostEquatables3)
+        
+//        let someStruct = SomeStruct(item: arrayOfAlmostEquatables)
+    }
+}
+
+fileprivate struct SomeStruct<Item: AlmostEquatable> {
+    let item: Item
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+extension Array where Element: AlmostEquatable {
+    static func~==(lhs: [Element], rhs: [Element]) -> Bool {
+        guard lhs.count == rhs.count else { return false }
+
+        for i in 0..<lhs.count {
+            if lhs[i] ~!= rhs[i] { return false }
+        }
+        
+        return true
+    }
+}
+
+//extension Array: AlmostEquatable where Element: AlmostEquatable {
+//}
