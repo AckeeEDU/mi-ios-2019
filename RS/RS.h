b498d3a347b8c6e26059c188b2a1895c2214cc57
pteasima <ptea.sima@gmail.com>
2019-03-06 08:29:42 +0100

    Initial Commit

---
+++ RS/RS.h
@@ -0,0 +1,19 @@
+//
+//  RS.h
+//  RS
+//
+//  Created by Petr Šíma on 06/03/2019.
+//  Copyright © 2019 Petr Šíma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RS.
+FOUNDATION_EXPORT double RSVersionNumber;
+
+//! Project version string for RS.
+FOUNDATION_EXPORT const unsigned char RSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RS/PublicHeader.h>
+
+
