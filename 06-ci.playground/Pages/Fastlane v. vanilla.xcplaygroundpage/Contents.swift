246213ecec298f718379c27ced57b403f93c2c8b
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-03-27 14:02:24 +0100

    Add comple CI playground

---
+++ 06-ci.playground/Pages/Fastlane v. vanilla.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+//: # CI
+//: ## Fastlane v. vanilla
+//:
+//: Zkusme spustit testy ☝️
+//: ```
+//: xcodebuild test -scheme mi-ios-2019 -workspace mi-ios-2019.xcworkspace -destination 'platform=iOS Simulator,name=iPhone XS Max,OS=12.1'
+//: ```
+//: 🙄
+//: ```
+//: fastlane scan --scheme mi-ios-2019
+//: ```
+//: 👌
+//:
+//: [Next](@next)
+
+
---
+++ 06-ci.playground/Pages/Fastlane.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+//: # CI
+//: ## Fastlane
+//:
+//: - set toolů (nejen) pro iOS vývoj
+//: - je to v ruby 😬
+//: - umí řešit prakticky celou flow vývoje appky
+//: - správa certifikátů a ostatních code signing credentials _(match)_
+//: - umí řešit dependencies (Cocoapods, Carthage, SwiftPM)
+//: - build aplikace _(gym)_
+//: - spouštění testů _(scan)_
+//: ```
+//: fastlane scan --scheme mi-ios-2019
+//: ```
+//: - deploy aplikace (HockeyApp/Crashlytics/Testflight/App Store/...)
+//: - interakce s App Store Connect _(deliver)_
+//: - umí sdružovat více akcí do tzv. **lanes**
+//:
+//: [Next](@next)
